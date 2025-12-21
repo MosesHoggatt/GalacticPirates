27d274fe2d5dfe811eb31cb9b14a10060fbecee3
Endless98 <endlessninetyeight@gmail.com>
2025-12-21 13:15:44 -0700

    Project initialization

---
+++ GalacticPirates/GalacticPirates.Build.cs
@@ -0,0 +1,44 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class GalacticPirates : ModuleRules
+{
+	public GalacticPirates(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"GalacticPirates",
+			"GalacticPirates/Variant_Horror",
+			"GalacticPirates/Variant_Horror/UI",
+			"GalacticPirates/Variant_Shooter",
+			"GalacticPirates/Variant_Shooter/AI",
+			"GalacticPirates/Variant_Shooter/UI",
+			"GalacticPirates/Variant_Shooter/Weapons"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
